cfa5798ff4857a4bdff48e833beb74c568ecc8bc
t3rr0r <mail@t3rr0r.com>
2016-04-01 11:14:47 -0400

    decouple tags & crate + edit title

--- src/content/js/vitamin.js
+++ src/content/js/vitamin.js
@@ -27,46 +27,105 @@
 
 	    var title = Elem.create({
 		tag: 'h2',
+		className: 'vacay-title',
 		text: data.displayTitle
 	    });
 	    elem.appendChild(title);
 
-	    var crate = Elem.create({
+	    var editTitle = Elem.create({
 		tag: 'button',
-		text: 'Crate'
+		className: 'vacay-edit-hide',
+		text: 'Edit Title',
+		parent: elem
 	    });
 
-	    data.crated = Utils.exists(data.craters, Vacay.username, 'username');
-		
+	    editTitle.onclick = function() {
+		elem.classList.toggle('vacay-editable', true);
+		title.contentEditable = 'true';
+		title.dataset.prev = Elem.text(title);
+		title.focus();
+	    };
+
+	    var saveTitle = Elem.create({
+		tag: 'button',
+		className: 'vacay-edit-show',
+		text: 'Save Title',
+		parent: elem
+	    });
+
+	    saveTitle.onclick = function() {
+		elem.classList.toggle('vacay-editable', false);
+		title.contentEditable = 'false';
+		var cb = function(err) {
+		    if (err) Log.error(err);
+		};
+
+		Vitamin.update(data.id, Elem.text(title), cb);
+	    };
+
+	    var cancelTitle = Elem.create({
+		tag: 'button',
+		className: 'vacay-edit-show',
+		text: 'Cancel',
+		parent: elem
+	    });
+
+	    cancelTitle.onclick = function() {
+		elem.classList.toggle('vacay-editable', false);
+		title.contentEditable = 'false';
+		title.innerHTML = title.dataset.prev;
+	    };
+
+	    var crate = Elem.create({
+		tag: 'button',
+		className: 'vacay-success',
+		text: 'Crate',
+		parent: elem
+	    });
+
 	    crate.onclick = function() {
 		var isActive = crate.classList.contains('active');
 		var cb = function(err) {
 		    if (err) crate.classList.toggle('active', isActive);
 		};
 
-		if (isActive) {
-		    self.uncrate(data.id, cb);
-		    elem.removeChild(elem.querySelector('.vacay-crx-tags'));
-		} else {
-		    self.crate(data.id, cb);
-		    elem.appendChild(Tags.render());
-		}
+		if (isActive) self.uncrate(data.id, cb);
+		else self.crate(data.id, cb);
+
 		crate.classList.toggle('active');
 	    };
-	    elem.appendChild(crate);
+	    data.crated = Utils.exists(data.craters, Vacay.username, 'username');
+	    if (data.crated) crate.classList.add('active');
 
-	    if (data.crated) {
-		crate.classList.add('active');
-		elem.appendChild(Tags.render());
+	    elem.appendChild(Tags.render());
 
-		var parent = elem.querySelector('.vacay-crx-tags .current');
-		if (data.tags.length) {
-		    data.tags.forEach(function(t) {
-			parent.appendChild(Tags.tag(t.value, { create: true }));
-		    });
-		}		
+	    var parent = elem.querySelector('.vacay-crx-tags .current');
+	    if (data.tags.length) {
+		data.tags.forEach(function(t) {
+		    parent.appendChild(Tags.tag(t.value, { create: true }));
+		});
+	    }
 
-	    }	    
+	    var h = Elem.create({
+		className: 'vacay-h vacay-_d vacay-edit-show',
+		parent: elem,
+		childs: [{
+		    tag: 'span',
+		    text: 'Title Suggestions'
+		}]
+	    });
+	    var list = Elem.create({
+		className: 'list vacay-edit-show',
+		parent: elem
+	    });
+
+	    data.hosts.forEach(function(h) {
+		Elem.create({
+		    className: 'i',
+		    text: h.vitamin_title,
+		    parent: list
+		});
+	    });
 
 	    return elem;
 	},
@@ -78,6 +137,18 @@
 		cb(null, res.data);
 	    }).error(function(res) {
 		cb(res);
+	    });
+	},
+
+	update: function(id, title) {
+	    if (!title) return;
+
+	    Vacay.api('/vitamin/' + id).put({
+		title: title
+	    }).success(function() {
+		//TODO
+	    }).error(function(err) {
+		Log.error(err, title);
 	    });
 	},
 
